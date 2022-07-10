d7c7a305ffb1e707784be7481b5bcf80c01230a6
Matias Ruiz <matiasignacioruiz@gmail.com>
2022-07-10 12:35:00 -0300

    archivos inicio

---
+++ curso git/main.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/hola', (req, res) => {
+    res.send('ruta Hola');
+});
+
+app.listen(3000, ()=> {
+    console.log('Example app listening on port 3000!');
+});
